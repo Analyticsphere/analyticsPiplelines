4b60a76a9815a1ae652293e61b3c27bc652d4ab1
danielruss <druss@mail.nih.gov>
2022-09-30 17:18:53 -0400

    everything back in...

--- stage/test_api.r
+++ stage/test_api.r
@@ -9,11 +9,11 @@ library(scales)
 library(dplyr)
 library(boxr)
 
-#source("cloud_run_helper_functions.r")
+source("cloud_run_helper_functions.r")
 
 # Report package availability to text file for debugging purposes
-#check_package_availability("bigrquery", "gridExtra", "plumber", "ggplot2", 
-#                           "gridExtra", "scales", "dplyr")
+check_package_availability("bigrquery", "gridExtra", "plumber", "ggplot2", 
+                           "gridExtra", "scales", "dplyr")
 
 #* heartbeat...for testing purposes only. Not required to run analysis.
 #* @get /
@@ -25,39 +25,39 @@ function(){return("alive")}
 #* Runs STAGE test script
 #* @get /test_api
 #* @post /test_api
-#function() {
+function() {
 
   # Change project and billing info as needed.
-#  project = "nih-nci-dceg-connect-stg-5519"  
-#  billing = "nih-nci-dceg-connect-stg-5519"
+  project = "nih-nci-dceg-connect-stg-5519"  
+  billing = "nih-nci-dceg-connect-stg-5519"
   
   # Designate bucket name (bucket must exist in GCP project) 
-#  bucket_name   <- 'test_analytics_bucket_jp' 
-#  output_folder <- 'output' # Do not change this! Must correspond to Dockerfile.
+  bucket_name   <- 'test_analytics_bucket_jp' 
+  output_folder <- 'output' # Do not change this! Must correspond to Dockerfile.
   
   # Simple query.
-#  query_rec <- "SELECT 117249500 AS RcrtUP_Age_v1r0 
-#               FROM `nih-nci-dceg-connect-prod-6d04.Connect.participants` 
-#               WHERE Connect_ID IS NOT NULL"
+  query_rec <- "SELECT 117249500 AS RcrtUP_Age_v1r0 
+               FROM `nih-nci-dceg-connect-prod-6d04.Connect.participants` 
+               WHERE Connect_ID IS NOT NULL"
   
   # BigQuery authorization. Should work smoothly on GCP without any inputs.
-#  bq_auth() 
+  bq_auth() 
   
   # Download some data
-#  rec_table <- bq_project_query(project, queryrec)
-#  rec_data <- bq_table_download(rec_table, bigint = "integer64")
-#  t <- head(rec_data) # Get just the top few lines of the table.
+  rec_table <- bq_project_query(project, queryrec)
+  rec_data <- bq_table_download(rec_table, bigint = "integer64")
+  t <- head(rec_data) # Get just the top few lines of the table.
   
   # Write a table to pdf as an example "report". 
   # Must include path to output folder in file name
-#  report_name = './output/report_table.pdf'
-#  pdf(report_name)           # Opens a PDF
-#  grid.table(t)              # Put table in PDF
-#  dev.off()                  # Closes PDF
+  report_name = './output/report_table.pdf'
+  pdf(report_name)           # Opens a PDF
+  grid.table(t)              # Put table in PDF
+  dev.off()                  # Closes PDF
   
   # Export output folder to bucket
-#  export_folder_contents_to_bucket(output_folder, bucket_name)
+  export_folder_contents_to_bucket(output_folder, bucket_name)
   
-#  return("All done.")
-#}
+  return("All done.")
+}
 
